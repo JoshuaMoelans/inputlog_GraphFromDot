cf10a6f3f695c6823d6d8e43c0ba353b442f0e27
JoshiDhima <joshikaposhi@hotmail.com>
2024-04-14 02:32:57 +0200

    add toggleable d3 view

--- standalone/scripts.js
+++ standalone/scripts.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var network, data;
+    var view, root, focus, svg, node, label;
     var nodeDict;
     var totalSize;
     var selectedNode;
@@ -26,12 +27,14 @@ document.addEventListener("DOMContentLoaded", function () {
     };
     var page = document.getElementById("page");
     var container = document.getElementById("mynetwork");
+    var d3container = document.getElementById("d3graph");
     var info = document.getElementById("infocontent");
     var buttons = document.getElementById("buttons");
     var originalInfoBoxHTML = document.getElementById('infocontent').innerHTML;
     var clusters = new Set();
     var dragging = false;
     var changedNodes = new Map();
+    var networkView = true;
     var defaultGraph = "digraph source_graph {\n" +
       "\trankdir=LR;\n" +
       "\tnode [shape=circle style=filled fixedsize=true fontsize=20];\n" +
@@ -103,11 +106,11 @@ document.addEventListener("DOMContentLoaded", function () {
         input.click();
     }
 
-    function saveDotFile() {
+    function createDotString() {
         // Get graph data from vis.js
         const nodes = network.body.data.nodes._data;
         const edges = network.body.data.edges._data;
-        
+
         // Build DOT string
         let dotString = 'digraph myGraph {\n';
         dotString += "\trankdir=LR;\n";
@@ -115,7 +118,6 @@ document.addEventListener("DOMContentLoaded", function () {
         dotString += "\tsize=\"15\";\n\n";
 
         nodes.forEach(node => {
-            console.log(node);
             var [label, weight] = [node.title, node.width]; // use title as stored label (actual label has been truncated) and width as weight
             clusterInfo = nodeDict[node.id].cluster ? ` cluster="${nodeDict[node.id].cluster}"` : "";
             dotString += `\tnode [width=${weight} fillcolor=\"${node.color.background}\" bordercolor=${node.color.border} label="${label}"${clusterInfo}] ${node.id};\n`;
@@ -126,6 +128,12 @@ document.addEventListener("DOMContentLoaded", function () {
             dotString += `\t${edge.from} -> ${edge.to} [label="${edge.label}" penwidth=${edge.label}];\n`;
         });
         dotString += '}';
+        
+        return dotString;
+    }
+
+    function saveDotFile() {
+        dotString = createDotString();
 
         var blob = new Blob([dotString], {type: "text/plain;charset=utf-8"});
         saveAs(blob, "network.gv");
@@ -381,12 +389,198 @@ document.addEventListener("DOMContentLoaded", function () {
         return colors;
     }
 
+    function graphvizToJSON(dotString) {
+        // Extract clusters and node widths
+        const clusters = [];
+        const clusterDict = {};
+        const nodeWidths = {};
+    
+        // Regular expression patterns for extracting cluster and node information
+        const nodePattern = /node\s+\[width=(\d+)\s+.*?\s+label="(.*?)"(?:\s+cluster="(.*?)")?\]\s+(\d+)/gs;
+    
+        // Function to find all matches in the string using a regex pattern
+        function findAllMatches(regex, text) {
+            const matches = [];
+            let match;
+            while ((match = regex.exec(text)) !== null) {
+            matches.push(match);
+            }
+            return matches;
+        }
+    
+        const nodeMatches = findAllMatches(nodePattern, dotString);
+    
+        for (const nodeMatch of nodeMatches) {
+            const nodeWidth = parseInt(nodeMatch[1]);
+            const nodeName = nodeMatch[2];
+            const clusterGroup = nodeMatch[3] || 'root';
+    
+            if (clusterGroup === 'root') {
+            clusters.push({
+                name: nodeName,
+                value: nodeWidth,
+            });
+            }
+            else {
+            if (clusterDict[clusterGroup] === undefined) {
+                clusterDict[clusterGroup] = {
+                name: clusterGroup,
+                children: [],
+                };
+            }
+    
+            nodeWidths[nodeName] = nodeWidth;
+            clusterDict[clusterGroup].children.push({
+                name: nodeName,
+                value: nodeWidth,
+            });
+            }
+        }      
+    
+        for (const clusterGroup in clusterDict) {
+            clusters.push(clusterDict[clusterGroup]);
+        }
+    
+        // Convert JSON data to a hierarchical format
+        const root = {
+            name: "root",
+            children: clusters,
+        };
+    
+        return d3.hierarchy(root).sum(d => d.value);
+    }  
+
+    function zoomTo(v) {
+        const k = parseFloat(d3container.style.width) / v[2];
+  
+        view = v;
+        
+        label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+        node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+        node.attr("r", d => d.r * k);
+    }
+  
+    function zoom(event, d) {
+        const focus0 = focus;
+
+        focus = d;
+
+        const transition = svg.transition()
+            .duration(event.altKey ? 7500 : 750)
+            .tween("zoom", d => {
+                const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+                return t => zoomTo(i(t));
+            });
+
+        label
+            .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+            .transition(transition)
+            .style("fill-opacity", d => d.parent === focus ? 1 : 0)
+            .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+            .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+    }
+
+    // Function to create the network visualization with a given DOT string
+    function createD3Graph() {
+        dotString = createDotString();
+
+        const data = graphvizToJSON(dotString);
+        let width = parseFloat(d3container.style.width);
+        let height = parseFloat(d3container.style.height);
+        let minSize = Math.max(width, height);
+    
+        const color = d3.scaleLinear()
+            .domain([0, 5])
+            .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+            .interpolate(d3.interpolateHcl);
+    
+        const pack = data => d3.pack()
+            .size([width, height])
+            .padding(3)
+            (d3.hierarchy(data)
+            .sum(d => d.value)
+            .sort((a, b) => b.value - a.value));
+    
+        root = pack(data);
+        focus = root;
+
+        svg = d3.select("#d3graph")
+            .attr("viewBox", `-${minSize / 2} -${minSize / 2} ${minSize} ${minSize}`)
+            .style("display", "block")
+            .style("background", color(0))
+            .style("cursor", "pointer")
+            .on("click", (event) => zoom(event, root));
+    
+        node = svg.append("g")
+            .selectAll("circle")
+            .data(root.descendants().slice(1))
+            .join("circle")
+            .attr("fill", d => d.children ? color(d.depth) : "white")
+            .attr("pointer-events", d => !d.children ? "none" : null)
+            .on("mouseover", function() { d3.select(this).attr("stroke", "#000"); })
+            .on("mouseout", function() { d3.select(this).attr("stroke", null); })
+            .on("click", (event, d) => focus !== d && (zoom(event, d), event.stopPropagation()));
+    
+        function getSize(d) {
+            var bbox = this.getBBox(),
+                cbbox = this.parentNode.getBBox(),
+                scale = Math.min(cbbox.width/bbox.width, cbbox.height/bbox.height);
+            d.scale = scale;
+        }
+        
+        // TODO: automatic font size scaling doesnt work yet
+        label = svg.append("g")
+            .style("font", "sans-serif")
+            .attr("pointer-events", "none")
+            .attr("text-anchor", "middle")
+            .selectAll("text")
+            .data(root.descendants())
+            .join("text")
+            .style("fill-opacity", d => d.parent === root ? 1 : 0)
+            .style("display", d => d.parent === root ? "inline" : "none")
+            .text(d => d.data.data.name)
+            .style("font-size", "30px")
+            .each(getSize)
+            .style("font-size", function(d) { return 30 * d.scale + "px"; });
+    
+        zoomTo([root.x, root.y, root.r * 2]);
+    }
+
+    function toggleView() {
+        if (networkView) {
+            networkView = false;
+
+            document.getElementById("info").style.display = "none";
+            document.getElementById("tasks").style.display = "none";
+
+            container.style.width = "0px";
+            container.style.height = "0px";
+            d3container.style.width = window.innerWidth * 0.94 + "px";
+            d3container.style.height = window.innerHeight * 0.85 + "px";
+
+            d3.select("#d3graph").selectAll("*").remove();
+            createD3Graph();
+        } else {
+            // Switch to network view
+            networkView = true;
+
+            document.getElementById("info").style.display = "block";
+            document.getElementById("tasks").style.display = "block";
+
+            container.style.width = window.innerWidth * 0.94 + "px";
+            container.style.height = window.innerHeight * 0.85 + "px";
+            d3container.style.width = "0px";
+            d3container.style.height = "0px";
+        }
+    }
+
     // Initial setup
     // TODO: Currently doesn't work if I don't call initializeNetwork() twice, not sure why
     initializeNetwork();
     initializeNetwork();
 
     // Event listeners
+    document.getElementById("toggleButton").addEventListener("click", toggleView);
     document.getElementById("clusterButton").addEventListener("click", createClusters);
     document.getElementById("uploadButton").addEventListener("click", loadDotFile);
     document.getElementById("saveButton").addEventListener("click", saveDotFile);
